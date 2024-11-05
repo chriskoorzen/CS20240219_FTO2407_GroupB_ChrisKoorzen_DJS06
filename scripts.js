d55416d81b9d4d48b7262183a454d56abbd3100d
Chris Koorzen <fckoorzen@gmail.com>
2024-11-05 14:52:56 +0200

    Export main data structures

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 // A list of provinces:
-const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+export const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
 
 // A list of names:
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+export const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
 
 // A list of products with prices:
-const products = [
+export const products = [
   { product: 'banana', price: "2" },
   { product: 'mango', price: 6 },
   { product: 'potato', price: ' ' },
